222743515da36bd192cb4cdc1bf45ea4532a9488
99cm <zyizyao@china-guide.com>
2018-03-20 23:32:02 -0400

    Update rails 5.1 activerecord migration support

--- db/migrate/20160219094745_add_sale_price_to_spree_variants.rb
+++ db/migrate/20160219094745_add_sale_price_to_spree_variants.rb
@@ -1,4 +1,4 @@
-class AddSalePriceToSpreeVariants < ActiveRecord::Migration
+class AddSalePriceToSpreeVariants < ActiveRecord::Migration[5.1]
   def change
     add_column :spree_variants, :sale_price, :decimal, precision: 8, scale: 2
   end
